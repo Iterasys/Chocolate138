75e3a22183736cd23243d026781b9aa90ecf94e9
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-06-01 22:36:22 -0300

    Início de Testes de API: Criação do testes de Api.txt, do pacote apitests e da classe Account;Inclusão no build.gradle.kts da dependência do REST-assured

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     testImplementation("org.testng:testng:7.7.1")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+
 }
 
 tasks.test {
---
+++ src/test/java/apitest/Account.java
@@ -0,0 +1,10 @@
+// 1 - Pacote
+package apitest;
+// 2 - Bibliotecas
+// 3 - Classe
+public class Account {
+    // 3.1 - Atributos
+
+    // 3.2 - Métodos e Funções
+
+}
