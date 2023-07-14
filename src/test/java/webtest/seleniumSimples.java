f6fd71aa8fbb71bf2d976ac9516f93dae224e3bd
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-07-13 22:36:42 -0300

    Completo o script simples com Selenium e criamos um arquivo de feature para a mesma funcionalidade

--- src/test/java/webtest/seleniumSimples.java
+++ src/test/java/webtest/seleniumSimples.java
@@ -3,13 +3,20 @@ package webtest;
 // 2 - Bibliotecas
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 // 3 - Classe
 public class seleniumSimples {
@@ -43,5 +50,56 @@ public class seleniumSimples {
     }
 
     // 3.5 Teste em Si
+    @Test
+    public void testarSelectBackdrop(){
+        // Abrir a página inicial do site SauceDemo
+        driver.get("https://www.saucedemo.com");
 
+        // Digitar o usuário e a senha
+        // Clicar no elemento antes de escrever
+
+        WebElement username = driver.findElement(By.id("user-name")); // Controla a caixa de texto
+        username.click(); // clica na caixa de texto username
+        username.clear(); // limpa a caixa de texto
+        username.sendKeys("standard_user"); // escreve na caixa (colar o texto)
+
+        // username.sendKeys(Keys.chord("standard_user")); // escreve na caixa (letra por letra)
+        // driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        driver.findElement(By.id("login-button")).click();
+
+        // Transição de página / Carregamento de nova página (lentidão)
+
+        // Verificar se estamos na página interna (se conseguimos entrar)
+        // Verifica a palavra "Products" em determinado elemento
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(), "Products");
+        // Verifica se está presente o elemento do carrinho de compras
+        assertTrue(driver.findElement(By.id("shopping_cart_container")).isDisplayed());
+
+        // Selecionar o produto que seria o id nº 4
+        driver.findElement(By.id("item_4_title_link")).click();
+        // Transição de tela para a página do produto
+
+        // Validar o nome e o valor
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_details_name.large_size")).getText(),
+                "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_details_price")).getText(),
+                "$29.99");
+
+        // Clicar no botão Adicionar no Carrinho
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+
+
+        // Clica no icone do Carrinho
+        driver.findElement(By.id("shopping_cart_container")).click();
+        // Outra transição/carregamento de página
+
+        // Verificar o Titulo da Página, nome do produto, quantidade e preço
+        assertEquals(driver.findElement(By.cssSelector("span.title")).getText(), "Your Cart");
+        assertEquals(driver.findElement(By.id("item_4_title_link")).getText(), "Sauce Labs Backpack");
+        assertEquals(driver.findElement(By.cssSelector("div.cart_quantity")).getText(), "1");
+        assertEquals(driver.findElement(By.cssSelector("div.inventory_item_price")).getText(), "$29.99");
+
+    }
 }
---
+++ src/test/resources/features/selectProduct.feature
@@ -0,0 +1,26 @@
+# #language: pt
+#  Funcionalidade: Selecionar Produto na Loja
+#    Cenario: Selecionar Produto com Sucesso
+#      Dado que acesso a loja SauceDemo
+#      Quando preencho o usuario e senha
+#      E clica em Login
+#      Entao exibe o titulo da pagina como "Products"
+#      E exibe o link do carrinho de compras
+#      Quando clica no produto "Sauce Labs Backpack"
+
+  Feature: Select Product in Store
+    Scenario: Selecting Product with Sucess
+      Given I access SauceDemo store
+      When I filled a user "standard_user" and password "secret_sauce"
+      And I click in Login
+      Then show page's title "Products"
+      And show cart's link
+      When I click in product "Sauce Labs Backpack"
+      Then I verify the product title "Sauce Labs Backpack"
+      And I verify the product price "$ 29.99"
+      When I click in Add to Cart
+      And I click in Cart icon
+      Then I verify the page's title "Your Cart"
+      And I verify the product title "Sauce Labs Backpack"
+      And I verify the quantity is "1"
+      And I verify the product price "$ 29.99"
